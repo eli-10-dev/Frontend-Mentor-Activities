d1a75abe03bfd9167f194b7fdde80428ce9b6da0
eli-10-dev <elijah.gaylan@gmail.com>
2025-09-11 13:36:07 +0800

    Added media queries, active states, image icon on header of each info panel, fixed styling for all panels, added dashboard updating based on button click.

--- (3) JavaScript Basics/(3) Time Tracking Dashboard/script.js
+++ (3) JavaScript Basics/(3) Time Tracking Dashboard/script.js
@@ -1,16 +1,21 @@
+const informationPanelsContainer = document.getElementById('information-panel-container');
+let dataArray = [];
+let selectedTimeFrame = 'daily';
+const timeFrameButtons = document.querySelectorAll('.filter-button');
+
+// Fused the event listeners into one
 // const dailyButton = document.getElementById('daily-button');
 // const weeklyButton = document.getElementById('weekly-button');
 // const monthlyButton = document.getElementById('monthly-button');
-const timeSpanButtonsContainer = document.getElementById('timespan-filter');
-const informationPanelsContainer = document.getElementById('information-panel-container');
-let selectedTimeFrame = 'daily';
 
 fetch('./data.json')
 .then(res => res.json())
 .then(data => {
-    console.log(data);
+    // console.log(data);
     // testFunction(data);
     displayInformation(data);
+    dataArray = JSON.parse(JSON.stringify(data));
+    // console.log("Data Array:", dataArray);
 })
 .catch(error => console.log("Error: ", error));
 
@@ -20,16 +25,20 @@ const displayInformation = (data) => {
             data.map(activity => {
                 return `
                     <div class="${activity.title.toLowerCase().replace(" ", "")} panel" id="panel"> 
+                        <div class="panel-header">
+                            <img class="activity-icon" src="images/icon-${activity.title.toLowerCase()}.svg" alt="${activity.title.toLowerCase()} icon">
+                        </div>
+                        
                         <section class="panel-content">
 
                             <section class="panel-indicator">
-                                <h1>${activity.title}</h1>
+                                <h1 class="activity-title">${activity.title} </h1>
                                 <img class="ellipsis" src="images/icon-ellipsis.svg" alt="ellipsis">
                             </section>
                             
                             <section>
                                 <section class="time-display">
-                                    <div>${activity.timeframes[selectedTimeFrame].current}hrs</div>
+                                    <div class="current-time">${activity.timeframes[selectedTimeFrame].current}hrs</div>
                                     <div>${
                                         (selectedTimeFrame === 'daily' 
                                             ? 'Yesterday' : selectedTimeFrame === 'weekly' 
@@ -61,3 +70,21 @@ const testFunction = (array) => {
         `);
     });
 };
+
+console.log(timeFrameButtons);
+
+for (const button of timeFrameButtons){
+    button.addEventListener('click', (e) => {
+        selectedTimeFrame = e.target.dataset.value;
+        // console.log(selectedTimeFrame);
+        displayInformation(dataArray);
+
+        for (const button of timeFrameButtons){
+            if (button === e.target){
+                button.classList.add('active');
+            } else {
+                button.classList.remove('active');
+            }
+        }
+    });
+}
